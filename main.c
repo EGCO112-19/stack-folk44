f3f8c4fc77d2dbc966542ab65a51c9be61cc154a
folk44 <krittawat.1079@gmail.com>
2021-03-03 12:51:58 +0000

    Completed!!

--- main.c
+++ main.c
@@ -8,22 +8,40 @@ int main(int argc, char **argv){
   int i,N,j;
   
   Stack s;
+  s.size=0;
+  s.top=NULL;
 
+  for(i=1;i<argc;i++){ //
+    N=0;//use to check conditions
+    for(j=0;j<strlen(argv[i]);j++){
+      switch (argv[i][j]){
+        case '{' :{
+          push(&s,'{');
+        };break;
+         
+         case '}' :{
+          if(pop(&s)!='{')
+            N=1;
+        };break;
+        
+        case '[' :{
+          push(&s, '[');
+        };break;
+         
+        case ']' :{
+          if(pop(&s)!='[')
+            N=1;
+        };break;
+      }
+      if(N==1) break;
+    }
 
- 
-  for(i=1;i<argc;i++){
-   
-     for(j=0;j<strlen(argv[i]);j++){
-       /* Use stack to help with the parentheses*/
-
-
-     }
-
-
+    if(N==1||s.size!=0)
+      printf("The paratheses do not match for %s \n",argv[i]);
+    else
+      printf("The paratheses match successfully for %s \n",argv[i]);
+    
+    pop_all(&s);
   }
-
-
-
-
-   return 0;
+  return 0;
 }
